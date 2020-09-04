73cdf7577ebbf8262383f039ff10b89055dde25d
nehemiahk <nehemiahkiv@gmail.com>
2020-09-04 15:46:16 +0300

    fixed navigation for mobile

--- src/components/Navbar/BurgerMenu.jsx
+++ src/components/Navbar/BurgerMenu.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
-import './navbar.css'
+import React, { useState } from "react"
+import "./navbar.css"
+import { Link } from "gatsby"
 
 const BurgerMenu = () => {
+  const [change, setChange] = useState(false)
 
-    const [change, setChange] = useState(false)
+  const toggleChange = () => {
+    setChange(!change)
+  }
 
-    const toggleChange = () => {
-        setChange(!change)
-    }
+  return (
+    <div class={`${change ? "change" : ""} container`} onClick={toggleChange}>
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
 
-    return (<div class={`${change ? 'change' : ''} container`} onClick={toggleChange}>
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-
-        {change &&
-            <div className='popout-menu'>
-                <a href='#header'>ME</a>
-                <a href='#about'>ABOUT</a>
-                <a href='#portfolio'>PORTFOLIO</a>
-                <a href='#contact'>CONTACT</a>
-            </div>
-        }
-
-    </div>)
+      {change && (
+        <div className="popout-menu">
+          <Link to={"/"}>HOME</Link>
+          <Link to={"/portfolio/"}>PORTFOLIO</Link>
+          <Link to={"/about/"}>ABOUT</Link>
+          <Link to={"/blog/"}>BLOG</Link>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default BurgerMenu
